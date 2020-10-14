e1ec8aa8a57c71aad7eca371e1e602fe11955771
Omar Andrade <andradeomar77@gmail.com>
2020-10-14 11:31:58 -0430

    Agregue modulo saludo a main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from saludo import saludar as sa
 v = True
 while v:
-	n = int(input('*******Bienvenido al programa que te saluda*******\n Que quieres hacer?\n1)SALUDAR\n0)SALIR'))
+	n = int(input('*******Bienvenido al programa que te saluda*******\n Que quieres hacer?\n1)SALUDAR\n0)SALIR\n'))
 	if n == 0:
 		v = False
-	
+	if n == 1:
+		v = False
+		sa()
+
 
---
+++ saludo.py
@@ -0,0 +1,3 @@
+def saludar(valor = None):
+	nombre = str(input('Cual es tu nombre?\n'))
+	print('HOLAAA!', nombre, 'que tengas un bonito día')
