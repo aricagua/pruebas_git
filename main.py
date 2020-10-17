44b5cfa32f293130a1e50eda33a53002e1269319
Aricagua Developer <aricagua.developer@gmail.com>
2020-10-17 10:59:57 -0430

    Se integro modulo desear suerte

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from saludo import saludar as sa
 from despedidas import despedida as de
+from suerte import suerte as su
 v = True
 while v:
-	n = int(input('*-*-*- BIENVENIDO AL PROGRAMA QUE TE SALUDA, DESPIDE Y DESEA SUERTE -*-*-*\n Que quieres hacer?\n1)SALUDAR\n2)DESPEDIR\n0)SALIR\n'))
+	n = int(input('*-*-*- BIENVENIDO AL PROGRAMA QUE TE SALUDA, DESPIDE Y DESEA SUERTE -*-*-*\n Que quieres hacer?\n1)SALUDAR\n2)DESPEDIR\n3)DESEAR SUERTE\n0)SALIR\n'))
 	if n == 0:
 		v = False
 	elif n == 1:
@@ -11,5 +12,8 @@ while v:
 	elif n == 2:
 		v = False
 		de()
+	elif n == 3:
+		v = False
+		su()
 
 
